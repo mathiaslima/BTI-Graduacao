1b7a297b0c0b6eb5d5b83ce50bbe0b330eafe0d8
Arturo Fonseca <49870466+arturo32@users.noreply.github.com>
2019-10-03 13:50:39 -0300

    Ajeitando a lógica, mas mantendo a sua essência
    Explicações no código!

--- ITP/Prova/provaITP.c
+++ ITP/Prova/provaITP.c
@@ -7,32 +7,62 @@ int main(){
 	int i,j, qtd;
 	char pal[65];
 	char tenta[65];
+	char c;
 	int cont=0;
+	int ok = 1;
 
-	
-	for (i = 0; i < ; ++i)
+	i = 0;
+
+	//Lê letra por letra enquanto transforma em letras maiúsculas ate que o usuário aparte enter
+	do
 	{
-		scanf(" %c", &pal[i]);
-	}
+		scanf("%c", &pal[i]);
+		pal[i] = (char)toupper(pal[i]);
+		i++;
+	}while(pal[i-1] != '\n'); 
+
+	//Marcando o final da string
+	pal[i] = '\0';
 	
 	scanf("%d", &qtd);
+
+	//Verifica se o caractere digitado se encontra na string pal
 	for (i = 0; i < qtd; ++i)
 	{
-		scanf(" %c", &tenta[i]);
-		if(toupper(pal[i])==toupper(tenta[i])){
-			cont++;
+		scanf(" %c", &c);
+		c = (char)toupper(c);
+		
+		if(strchr(pal, c) == NULL)
+		{
+			ok = 0;
+		}
+		else
+		{
+			tenta[i] = (char)(*strchr(pal, c));
 		}
 	}
-	if (qtd == cont)
+	//O else serve para prevenir que um ponteiro NULL seja convertido em caractere
+	
+	//Verifica se as tentivas citaram todas as letras de pal
+	if(ok)
+	{
+		for (i = 0; i < strlen(pal)-1; ++i)
+		{
+			if(strchr(tenta, pal[i]) == NULL && pal[i] != ' ')
+			{
+				ok = 0;
+			}
+		}
+	}	
+
+	if (ok)
 	{
 		printf("Bucho Cheio!\n");
-	}else{
+	}
+	else
+	{
 		printf("Cabeças vao rolar!\n");
 	}
-
-	printf("%s\n", pal);
-	printf("%d\n", cont);
-	printf("%s\n", tenta);
 
 	return 0;
 }
